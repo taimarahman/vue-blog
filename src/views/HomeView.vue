b1d80475f79576851be31869697815cf04e56c94
Taima Rahman <taimarh@gmail.com>
2024-05-04 13:35:27 +0600

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<script setup>
+const posts = [
+  {
+
+  }
+]
+</script>
+
+<template>
+  <div class="wrapper">
+    <div class="header">
+      <span>Written by Name on date
+        <div>
+          <button class="del material-icons"></button>
+          <button class="save material-icons"></button>
+        </div>
+      </span>
+    </div>
+  </div>
+</template>
